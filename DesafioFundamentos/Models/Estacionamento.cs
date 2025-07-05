82a4e9f85ec130a56eddc62b75b51e2251280e03
Eddie Mauricio <huedoctor35@gmail.com>
2025-07-05 12:14:09 -0300

    Resolvendo comentarios

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,8 +14,7 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            //Implementado!!!!
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine();
             veiculos.Add(placa);
@@ -25,8 +24,7 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
+            //Implementado!!!!
 
             string placa = "";
             placa = Console.ReadLine();
@@ -36,9 +34,7 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+                //Implementado
                 int horas = 0;
                 decimal valorTotal = 0;
                 string entrada = Console.ReadLine();
@@ -47,8 +43,7 @@ namespace DesafioFundamentos.Models
                     horas = horascovertidas;
                     valorTotal = precoInicial + precoPorHora * horas;
 
-                    // TODO: Remover a placa digitada da lista de veículos
-                    // *IMPLEMENTE AQUI*
+                    //Implementado
                     Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
                     veiculos.Remove(placa);
                 }
